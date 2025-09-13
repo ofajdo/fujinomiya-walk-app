2f7e6f415e4ed403b5061929554a0dde3a0886f7
あああああ <kiuchiryou@icloud.com>
2025-09-14 07:32:52 +0900

    大文字小文字

---
+++ src/components/location/Detail.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import { Overview } from "./Overview";
+
+import { Prisma } from "@prisma/client";
+
+import { LocationsRoad } from "../map/LocationsMapLoad";
+
+type Location = Prisma.LocationGetPayload<{
+  include: {
+    course: {
+      include: {
+        routes: true;
+        locations: {
+          include: {
+            place: true;
+          };
+        };
+      };
+    };
+    topic: {
+      include: { topic: true };
+    };
+    culture: {
+      include: { culture: true };
+    };
+  };
+}>;
+
+export function Detail({ location }: { location: Location }) {
+  return (
+    <div className="flex flex-col gap-3">
+      <Overview location={location}>
+        <>
+          <ul className="flex flex-wrap gap-2 justify-around">
+            {location?.culture.map((cul, index) => (
+              <li key={index} className="text-sm text-red-600">
+                ・{cul?.culture.name}
+              </li>
+            ))}
+            {location?.topic?.map((top, index) => (
+              <li key={index} className="text-sm text-blue-600">
+                ・{top?.topic.name}
+              </li>
+            ))}
+          </ul>
+        </>
+      </Overview>
+      <div>
+        <p className="p-2 text-gray-700 text-sm">{location?.description}</p>
+      </div>
+      <div className="h-64 w-full">
+        <LocationsRoad
+          course={location.course}
+          location_index={location.number}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/location/Overview.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+
+import { Prisma } from "@prisma/client";
+type Location = Prisma.LocationGetPayload<{
+  include: {
+    course: true;
+  };
+}>;
+export function Overview({
+  location,
+  children,
+}: {
+  location: Location;
+  children: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="flex flex-wrap">
+      <div
+        className="w-36 max-w-[30%] flex items-center"
+        onClick={() => setIsOpen(true)}
+      >
+        {location?.image && (
+          <Image
+            src={location.image}
+            alt={`${location.title}｜${location.description}`}
+            width={240}
+            height={240}
+            className="rounded-lg"
+          />
+        )}
+      </div>
+      <div className="flex-1 p-1 flex flex-col justify-around gap-2">
+        <p className="text-center text-xs md:text-sm line-clamp-1">
+          <span className="text-sm font-bold pr-2 font-mono">
+            {location?.course.name}-{location?.number}
+          </span>
+          {location?.course.title}
+        </p>
+        <h2 className="font-medium text-lg md:text-2xl text-center text-balance cursor-pointer hover:underline line-clamp-2">
+          <Link href={`/location/${location?.id}`}>{location?.title}</Link>
+        </h2>
+        <div className="flex justify-center flex-wrap">{children}</div>
+      </div>
+      {/* モーダル */}
+      {isOpen && (
+        <div
+          className="fixed p-4 inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[2500]"
+          onClick={() => setIsOpen(false)}
+        >
+          <div
+            className="bg-white p-2 max-w-lg w-full"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Image
+              src={location.image}
+              alt={`${location.title}｜${location.description}`}
+              width={240}
+              height={240}
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
