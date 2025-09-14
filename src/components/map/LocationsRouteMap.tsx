fe45446c3b190126777f294595afe054585e8c0f
あああああ <kiuchiryou@icloud.com>
2025-09-14 16:07:34 +0900

    滑らかに移動

--- src/components/map/LocationsRouteMap.tsx
+++ src/components/map/LocationsRouteMap.tsx
@@ -46,7 +46,7 @@ function ChangeMapCenter({
 
 const currentLocationIcon = L.divIcon({
   html: ``,
-  className: "bg-blue-600 w-[18px] h-[18px] rounded-full border-2 border-white",
+  className: "bg-blue-600 rounded-full border-2 border-white",
   iconSize: [18, 18],
   iconAnchor: [9, 9],
 });
@@ -64,8 +64,9 @@ function StopTrackingOnMove({
 }
 
 const arrowIcon = L.divIcon({
-  html: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><rect width="16" height="16" fill="none"/><g fill="currentColor"><path d="M9.5 1.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0M6.44 3.752A.75.75 0 0 1 7 3.5h1.445c.742 0 1.32.643 1.243 1.38l-.43 4.083a1.8 1.8 0 0 1-.088.395l-.318.906l.213.242a.8.8 0 0 1 .114.175l2 4.25a.75.75 0 1 1-1.357.638l-1.956-4.154l-1.68-1.921A.75.75 0 0 1 6 8.96l.138-2.613l-.435.489l-.464 2.786a.75.75 0 1 1-1.48-.246l.5-3a.75.75 0 0 1 .18-.375l2-2.25Z"/><path d="M6.25 11.745v-1.418l1.204 1.375l.261.524a.8.8 0 0 1-.12.231l-2.5 3.25a.75.75 0 1 1-1.19-.914zm4.22-4.215l-.494-.494l.205-1.843l.006-.067l1.124 1.124h1.44a.75.75 0 0 1 0 1.5H11a.75.75 0 0 1-.531-.22Z"/></g></svg>`,
-  className: "text-white bg-blue-600 p-[4px] rounded-full",
+  html: `<svg xmlns="http://www.w3.org/2000/svg" className="h-[0.75em] w-[0.75em]" width="16" height="16" viewBox="0 0 16 16"><rect width="16" height="16" fill="none"/><g fill="currentColor"><path d="M9.5 1.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0M6.44 3.752A.75.75 0 0 1 7 3.5h1.445c.742 0 1.32.643 1.243 1.38l-.43 4.083a1.8 1.8 0 0 1-.088.395l-.318.906l.213.242a.8.8 0 0 1 .114.175l2 4.25a.75.75 0 1 1-1.357.638l-1.956-4.154l-1.68-1.921A.75.75 0 0 1 6 8.96l.138-2.613l-.435.489l-.464 2.786a.75.75 0 1 1-1.48-.246l.5-3a.75.75 0 0 1 .18-.375l2-2.25Z"/><path d="M6.25 11.745v-1.418l1.204 1.375l.261.524a.8.8 0 0 1-.12.231l-2.5 3.25a.75.75 0 1 1-1.19-.914zm4.22-4.215l-.494-.494l.205-1.843l.006-.067l1.124 1.124h1.44a.75.75 0 0 1 0 1.5H11a.75.75 0 0 1-.531-.22Z"/></g></svg>`,
+  className:
+    "text-white bg-blue-600 p-[4px] rounded-full transition-all duration-500",
   iconSize: [24, 24],
   iconAnchor: [12, 12],
 });
@@ -84,7 +85,7 @@ function MovingMarker({
 
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % route.length);
-    }, speed); // 0.5秒ごとに移動
+    }, speed);
 
     return () => clearInterval(interval);
   }, [route]);
@@ -153,7 +154,7 @@ function RouteMap({ course }: { course: Course }) {
         minZoom={10}
         maxZoom={18}
         center={route[0]}
-        zoom={17}
+        zoom={16}
         className="h-full w-full"
       >
         <TileLayer
